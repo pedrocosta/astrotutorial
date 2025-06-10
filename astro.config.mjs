21509ad9e92c89c1733540b6c89d617961425dca
Pedro Costa <eng.pedrocosta@gmail.com>
2025-06-10 15:01:13 +0100

    first astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://strotutorial2025.netlify.app"
+  site: "https://strotutorial2025.netlify.app",
+  integrations: [preact()]
 });
